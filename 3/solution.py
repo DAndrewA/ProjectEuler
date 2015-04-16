6058b282004b91fd165c2df254a109682b6a40b2
DAndrewA <Andrew.Martin@bourne-grammar.lincs.sch.uk>
2015-04-16 21:34:32 +0100

    for loop being awkward

--- 3/solution.py
+++ 3/solution.py
@@ -2,10 +2,11 @@ primeFactors = []
 number = 600851475143
 while number > 2:
     for x in range(number):
-        try:
-            if number % x == 0:
-                primeFactors.append(x)
-                number = number / x
-                break
+        if x == 0:
+            x = 1
+        if number % x == 0:
+            primeFactors.append(x)
+            number = number / (x)
+            break
 
 print(primeFactors)
