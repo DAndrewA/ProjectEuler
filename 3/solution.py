6a45f845bab2cd85144f88c9cbd2f1ce66d4ed99
Andrew Martin <Andrew.Martin@bourne-grammar.lincs.sch.uk>
2016-03-16 10:06:11 +0000

    updated 3.py to work

--- 3/solution.py
+++ 3/solution.py
@@ -3,7 +3,7 @@ number = 600851475143
 while number > 2:
     for x in range(number):
         if x == 0:
-            x = 1
+            x = 2
         if number % x == 0:
             primeFactors.append(x)
             number = number / (x)
