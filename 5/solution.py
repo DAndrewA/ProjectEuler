bc15f2a562fafbb65fcce7e1dc89d44cfc90c71f
DAndrewA <andrew.martin@bourne-grammar.lincs.sch.uk>
2017-06-04 17:46:50 +0100

    completed task 5

--- 5/solution.py
+++ 5/solution.py
@@ -1,12 +1,44 @@
-numberFound = False
-# last number known to be not divisible
-number = 45000
-print("RUNNING")
-while numberFound == False:
-    remainder = 0
-    for i in range(1,21):
-        remainder += number % i
-    if remainder == 0:
-        print(number)
-        numberFound = True
-    number += 1
+highestInteger = 20
+
+def getPrimeFactors(n):
+    primeFactors = {}
+    while n > 1:
+        p = True
+        i = 1
+        while p:
+            i += 1
+            if n % i == 0:
+                p = False
+                if i in primeFactors:
+                    primeFactors[i] += 1
+                else:
+                    primeFactors[i] = 1
+                n = n//i
+                break
+            if n == int(n**0.5):
+                p = False
+        if p:
+            primeFactors[n] = 1
+            n = 1
+    print(primeFactors)
+    return primeFactors
+
+primeFactorsArr = []
+productFactors = {}
+# getting prime factors of n from 1 to 20
+for i in range(1,highestInteger+1):
+    primeFactorsArr.append(getPrimeFactors(i))
+# getting the highest power of each prime factor accross all the sets
+for factorSet in primeFactorsArr:
+    for factor in factorSet:
+        if factor not in productFactors:
+            productFactors[factor] = factorSet[factor]
+        else:
+            if factorSet[factor] > productFactors[factor]:
+                productFactors[factor] = factorSet[factor]
+# creating the product
+print("productFactors : " , productFactors)
+product = 1
+for factor in productFactors:
+    product = product * (factor**productFactors[factor])
+print(product)
